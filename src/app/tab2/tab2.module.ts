0ed8528a68bfde629baba551b1a96657f56709d6
Pranav <pranavam.s@gmail.com>
2019-02-09 18:00:43 +0530

    Initial Version

---
+++ src/app/tab2/tab2.module.ts
@@ -0,0 +1,25 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab2Page } from './tab2.page';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([{ path: '', component: Tab2Page }])
+  ],
+  declarations: [Tab2Page]
+})
+
+export class Tab2PageModule {
+
+  public subjectClicked : boolean = true;
+
+  public toggleSubject() {
+    this.subjectClicked = !this.subjectClicked;
+  }
+}
